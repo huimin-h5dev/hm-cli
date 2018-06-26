be17f6200767fc48e28f3d743d6599d31887fbd7
Huang Tao <huangtao2@huimin100.cn>
2018-06-26 18:14:12 +0800

    update

--- bin/hm-cli.js
+++ bin/hm-cli.js
@@ -9,15 +9,17 @@ program
     .version('1.0.0')
     .option('-i, init [name]', '初始化中商惠民前端项目')
     .parse(process.argv);
-
+console.log(program.init)
 if (program.init) {
     const spinner = ora('正在从github下载中商惠民前端项目模板').start();
-    download('codexu/x-build', program.init, function (err) {
+    download('huimin-h5dev/hmfe-template', program.init, function (err) {
         if(!err){
             // 可以输出一些项目成功的信息
             console.info(chalk.blueBright('下载成功'));
         }else{
             // 可以输出一些项目失败的信息
+            console.info(chalk.red('下载失败'));
         }
+        spinner.stop();
     })
 }
